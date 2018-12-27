116d2cd6f10b8f1291e0db6dad846b08dc88b1ae
tec-goblin <nikolaskp@outlook.com>
2018-12-27 18:23:40 +0200

    first attempt to 3.4.0

--- index.d.ts
+++ index.d.ts
@@ -1,96 +1,72 @@
 import { EventEmitter } from 'events';
-import { Readable } from 'stream';
 
-export interface Bindings {
-  [key: string]: any;
+declare namespace driver {//do we need namespace?
+  export class Client {
+    constructor(url:string, options?: ConnectionOptions);
+    open(): Promise<void>;//to confirm void in connection
+    close(): Promise<void>;
+    submit<Τ>(message: (process.Bytecode|string), bindings: Bindings): Promise<{err: Error | null, results: ResultSet<Τ>}>;
+  }
+
+  export class Connection extends EventEmitter {
+    constructor(url:string, options?: ConnectionOptions);
+    open(): Promise<void>;//to confirm void in connection
+    close(): Promise<void>;
+    submit<Τ>(message: (process.Bytecode|string), bindings: Bindings): Promise<{err: Error | null, results: ResultSet<Τ>}>;
+  }
+
+  export interface ResultSet<T> {
+    items: Array<T>,
+    attributes: Map<string, any>,
+    length: number
+  }
+
+  export interface ConnectionOptions {
+      ca?: string | string[] | Buffer | Buffer[], 
+      cert?: string | string[] | Buffer | Buffer[],
+      pfx?: (String|Buffer),
+      mimeType?: string,
+      headers?: {[index: string]: string},
+      pingEnabled?: boolean,
+      pingInterval?: number,
+      pongTimeout?: number,
+      connectOnStartup?: boolean,
+      rejectUnauthorized?: boolean,
+      traversalSource?: string,
+      reader?: structure.GraphSONReader,
+      writer?: structure.GraphSONWriter,
+      authenticator?: Authenticator
+  }
+
+  export interface Bindings {
+    [key: string]: any;
+  }
+  export interface Authenticator {
+    evaluateChallenge(challenge: string): any; //can be improved
+  }
 }
 
-export interface QueryObject {
-  gremlin: string;
-  bindings: Bindings;
+declare namespace process {
+  export type sourceName = 'with' | 'withBulk' | 'withPath' | 'withSack' | 'withSideEffect' | 'withStrategies' | 'withoutStrategies';
+  export type stepName = 'E' | 'V' | 'addE' | 'addV' | 'inject' | 'io';
+  export class Bytecode {
+    addSource(name: sourceName, values: Array<object>): Bytecode;
+    addStep(name: stepName, values: Array<object>): Bytecode;
+    toString():string;
+  }
 }
 
-export class WebSocketGremlinConnection extends EventEmitter {
-  open: boolean;
-  sendMessage (message: string): void;
-  handleMessage (message: string): void;
+declare namespace structure {
+  export class GraphSONWriter {
+    constructor(options?: SerializationOptions)
+  }
+  export class GraphSONReader {
+    constructor(options?: SerializationOptions)
+  }
+  interface SerializationOptions {
+    serializers?: {[index: string]: Serializer};
+  }
+  export interface Serializer {
+    serialize(item: any): {typeKey: string, valueKey: any};
+  }
 }
-
-export class MessageStream extends Readable {}
-
-export class GremlinClient extends EventEmitter {
-  port: number;
-  host: string;
-  options: ClientOptions;
-  useSession: boolean;
-  sessionId: string;
-  connected: boolean;
-  connection: WebSocketGremlinConnection;
-
-  execute <T> (script: QueryObject, callback: (err: Error | null, results: T[]) => void): void;
-  execute <T> (script: string, callback: (err: Error | null, results: T[]) => void): void;
-  execute <T> (script: string, bindings: Bindings, callback: (err: Error | null, results: T[]) => void): void;
-  execute <T> (script: string, bindings: Bindings, message: any, callback: (err: Error | null, results: T[]) => void): void;
-
-  messageStream (script: QueryObject): MessageStream;
-  messageStream (script: string): MessageStream;
-  messageStream (script: string, bindings: Bindings): MessageStream;
-  messageStream (script: string, bindings: Bindings, message: any): MessageStream;
-
-  readable (script: QueryObject): Readable;
-  readable (script: string): Readable;
-  readable (script: string, bindings: Bindings): Readable;
-  readable (script: string, bindings: Bindings, message: any): Readable;
-}
-
-export interface ClientOptions {
-  /**
-   * Whether to use sessions or not (default: `false`).
-   */
-  session?: boolean;
-  /**
-   * The script engine to use on the server, see your gremlin-server.yaml file (default: `"gremlin-groovy"`).
-   */
-  language?: string;
-  /**
-   * The name of the "operation" to execute based on the available OpProcessor (default: `"eval"`).
-   */
-  op?: string;
-  /**
-   * The name of the OpProcessor to utilize (default: `""`).
-   */
-  processor?: string;
-  /**
-   * Mime type of returned responses, depending on the serializer (default: `"application/json"`).
-   */
-  accept?: string;
-  /**
-   * A custom URL connection path if connecting to a Gremlin server behind a WebSocket proxy.
-   */
-  path?: string;
-}
-
-export function createClient (port: number, host: string, options?: ClientOptions): GremlinClient;
-export function createClient (port: number, options?: ClientOptions): GremlinClient;
-export function createClient (options?: ClientOptions): GremlinClient;
-
-export interface QueryPromise <T> extends Promise<T> {
-  readonly query: string;
-}
-
-export interface TemplateTag {
-  <T> (gremlinChunks: TemplateStringsArray, ...values: any[]): QueryPromise<T[]>;
-}
-
-export function makeTemplateTag (client: GremlinClient): TemplateTag;
-
-export interface Functions {
-  [key: string]: (...args: any[]) => QueryObject;
-}
-
-export interface BoundFunctions {
-  [key: string]: <T> (...args: any[]) => QueryPromise<T[]>;
-}
-
-export function bindForClient (client: GremlinClient, functions: Functions): BoundFunctions;
-
